c4f3cf2ad489389cdde9924e750dc496097eaaab
MartinSundin <sundin83martin@gmail.com>
2025-12-03 14:21:38 +0000

    Problem 3b

--- problem3a.py
+++ problem3a.py
@@ -1,11 +1,7 @@
-import heapq
-
 with open('input3.txt','r') as f:
     batteries = [s.strip() for s in f.readlines()]
 
-# batteries = '987654321111111,811111111111119,234234234234278,818181911112111'.split(",")
-
-# Get the maximum power from each battery
+# Get the maximum power from each batterybank
 total = 0
 for row in batteries:
     nums = [int(b) for b in row]
@@ -15,8 +11,6 @@ for row in batteries:
         bmax = bmax*10 + max(nums[i+1:])
     else:
         bmax = bmax + 10*max(nums[:i])
-
-    # print(row, bmax)
     total += bmax
 
 print(f"The maximum sum is {total}")
---
+++ problem3b.py
@@ -0,0 +1,21 @@
+import heapq
+
+with open('input3.txt','r') as f:
+    batteries = [s.strip() for s in f.readlines()]
+
+# Get the maximum power from each batterybank
+total = 0
+for row in batteries:
+    # traverse list backwards, keeping only the 12 largest numbers
+    result = ""
+    for c in reversed(row):
+        if len(result) < 12:
+            new_num = c + result
+        else:
+            i = result.index(min(result))
+            new_num = max(c + result[:i] + result[i+1:] for i in range(12))
+        if result == '' or int(new_num) >= int(result):
+            result = new_num
+    total += int("".join([str(c) for c in result]))
+
+print(f"The maximum sum is {total}")
